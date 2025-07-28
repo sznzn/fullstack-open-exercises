efb3e4613ad527be088e04deefd37f89d2b3e8e6
Xin <lu0045@hotmail.com>
2025-07-28 14:52:16 +0200

    express

---
+++ part3/index.js
@@ -0,0 +1,95 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+
+let persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/info', (req, res) => {
+  const currentTime = new Date().toLocaleString();
+  const personCount = persons.length;
+  res.send(`<p>Phonebook has info for ${personCount} people</p><p>${currentTime}</p>`);
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  persons = persons.filter(person => person.id !== id);
+  res.status(204).end();
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ 
+      error: 'name and number are required'
+    });
+  }
+
+  const existingPerson = persons.find(person => person.name === body.name);
+  if (existingPerson) {
+    return res.status(400).json({ 
+      error: 'name must be unique' 
+    });
+  }
+
+  const newPerson = {
+    id: Math.floor(Math.random() * 1000000),
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(newPerson);
+  res.json(persons);
+})
+
+app.put('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const body = req.body;
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    person.name = body.name || person.name ;
+    person.number = body.number || person.number;
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+})
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
